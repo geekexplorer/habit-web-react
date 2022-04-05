12f580d9359b251584a5280d9cef118b45df8a14
GeekExplorer <lenny.self@gmail.com>
2022-04-05 15:06:21 -0700

    Fixed issue with input field value being overwritten by initial state.

--- src/components/Habit/HabitForm/HabitForm.tsx
+++ src/components/Habit/HabitForm/HabitForm.tsx
@@ -1,26 +1,39 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, createRef } from 'react';
 
 import css from './HabitForm.module.css';
 
 import Button from '../../UI/Button';
 
 const HabitForm: React.FC<{ onSubmit: () => void }> = (props) => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState();
   const [duration, setDuration] = useState<number>();
 
-  const validateForm = () => {};
+  const nameRef = createRef<HTMLInputElement>();
+  const durationRef = createRef<HTMLInputElement>();
+
+  const handleSubmit = () => {
+    const name = nameRef.current!.value;
+    const duration = +durationRef.current!.value;
+    console.log(name, duration);
+  };
 
   return (
     <div className={css['form-container']}>
       <form onSubmit={props.onSubmit} className={css['habit-form']}>
         <h3 className={css['habit-form__title']}>Create New Habit</h3>
         <label htmlFor='name'>Name</label>
-        <input type='text' name='name' id='name' value={name} />
+        <input ref={nameRef} type='text' name='name' id='name' value={name} />
         <label htmlFor='duration'>Duration</label>
-        <input type='number' min='7' max='31' value={duration} />
+        <input
+          ref={durationRef}
+          type='number'
+          min='7'
+          max='31'
+          value={duration}
+        />
       </form>
       <div className={css['button-container']}>
-        <Button onClick={validateForm}>Create New Habit</Button>
+        <Button onClick={handleSubmit}>Create New Habit</Button>
       </div>
     </div>
   );
