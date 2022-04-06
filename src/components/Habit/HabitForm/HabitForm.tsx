c8164d0bc8c517744864a9ef63ae1cd1457bc2a2
GeekExplorer <lenny.self@gmail.com>
2022-04-06 12:44:00 -0700

    Added basic form validation.

--- src/components/Habit/HabitForm/HabitForm.tsx
+++ src/components/Habit/HabitForm/HabitForm.tsx
@@ -3,11 +3,14 @@ import { Fragment, useState, createRef } from 'react';
 import css from './HabitForm.module.css';
 
 import Button from '../../UI/Button';
+import { stringify } from 'querystring';
+import { setDefaultResultOrder } from 'dns/promises';
 
 const HabitForm: React.FC<{ onSubmit: () => void }> = (props) => {
   const [name, setName] = useState();
   const [startDate, setStartDate] = useState<Date>();
   const [duration, setDuration] = useState<number>();
+  const [error, setError] = useState<string>();
 
   const nameRef = createRef<HTMLInputElement>();
   const startDateRef = createRef<HTMLInputElement>();
@@ -17,7 +20,36 @@ const HabitForm: React.FC<{ onSubmit: () => void }> = (props) => {
     const name = nameRef.current!.value;
     const startDate = new Date(startDateRef.current!.value);
     const duration = +durationRef.current!.value;
+
+    if (!validateFormData(name, startDate, duration)) {
+      return;
+    }
+
+    setError(undefined);
     console.log(name, startDate, duration);
+  };
+
+  const validateFormData = (
+    name: string,
+    startDate: Date,
+    duration: number
+  ): boolean => {
+    if (name.length < 1) {
+      setError('You must enter a habit name.');
+      return false;
+    }
+
+    if (!startDate) {
+      setError('You must enter a valid start date.');
+      return false;
+    }
+
+    if (duration < 7 || duration > 31) {
+      setError('You must enter a valid duration between 7 and 31 days.');
+      return false;
+    }
+
+    return true;
   };
 
   const getDateInputFormat = (date: Date) => {
@@ -32,6 +64,7 @@ const HabitForm: React.FC<{ onSubmit: () => void }> = (props) => {
     <div className={css['form-container']}>
       <form onSubmit={props.onSubmit} className={css['habit-form']}>
         <h3 className={css['habit-form__title']}>Create New Habit</h3>
+        {error && <div className={css['error-message']}>{error}</div>}
         <label htmlFor='name'>Name</label>
         <input ref={nameRef} type='text' name='name' id='name' value={name} />
         <label htmlFor='name'>Name</label>
