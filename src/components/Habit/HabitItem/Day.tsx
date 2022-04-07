8b9f9cae935d341ec689cfb5fbf8bda522850129
GeekExplorer <lenny.self@gmail.com>
2022-04-06 20:23:24 -0700

    Icons and styling. - Added FontAwesome icons - Fixed issues with styling not being applied

--- src/components/Habit/HabitItem/Day.tsx
+++ src/components/Habit/HabitItem/Day.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { regular } from '@fortawesome/fontawesome-svg-core/import.macro';
+
 import css from './Day.module.css';
 import DayModel from '../../../models/DayModel';
 import dayjs from 'dayjs';
@@ -8,8 +11,21 @@ dayjs.extend(localizedFormat);
 const Day: React.FC<{ day: DayModel }> = (props) => {
   return (
     <div className={css.day}>
-      <div>{props.day.completed.toString()}</div>
-      <div>{dayjs(props.day.date).format('L').split('/').slice(0, 2).join('/')}</div>
+      <div className={`${css['icon']} ${props.day.completed && css.completed}`}>
+        {props.day.completed ? (
+          <FontAwesomeIcon icon={regular('circle-check')} />
+        ) : (
+          <FontAwesomeIcon icon={regular('circle')} />
+        )}
+      </div>
+      <div className={css['day-date']}>
+        {dayjs(props.day.date)
+          .format('L')
+          .split('/')
+          .slice(0, 2)
+          .map((part) => +part)
+          .join('/')}
+      </div>
     </div>
   );
 };
--- src/components/Habit/HabitItem/HabitDetail.tsx
+++ src/components/Habit/HabitItem/HabitDetail.tsx
@@ -1,4 +1,6 @@
 import { Fragment } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 import css from './HabitDetail.module.css';
 
@@ -12,7 +14,14 @@ const HabitDetail: React.FC<{ habit: HabitModel }> = (props) => {
           <h3>{props.habit.title}</h3>
         </div>
         <div className={css['habit-detail__start-date']}>{new Date(props.habit.dateStarted).toLocaleDateString()}</div>
-        <div className={css['habit-detail__actions']}>Edit | Delete</div>
+        <div className={css['habit-detail__actions']}>
+          <div>
+            <FontAwesomeIcon icon={regular('pen-to-square')} />
+          </div>
+          <div>
+            <FontAwesomeIcon icon={regular('trash-can')} />
+          </div>
+        </div>
       </div>
     </Fragment>
   );
