b4d68c38c5b3b90d542a60b269e0f33f126b3296
Lenny Self <lenny.self@gmail.com>
2022-06-08 08:20:33 -0700

    update config and fix filename

--- src/controllers/Habitcontroller.ts
+++ src/controllers/Habitcontroller.ts
@@ -1,7 +1,7 @@
 import { REST } from './RESTHelpers';
 import HabitModel from '../models/HabitModel';
 
-const API_URL = 'http://budgie-i7:5091/api/habit';
+const API_URL = 'http://ubuntu-7010:5091/api/habit';
 
 export enum RequestStatus {
   Ok,
